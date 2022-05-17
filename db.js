7f966e61c02613602e94d500392c4bc4bd9832de
Finoana71 <finoanamandresy71@gmail.com>
2022-05-17 17:01:38 +0300

    Create db.js

---
+++ db.js
@@ -0,0 +1,46 @@
+// categorie 
+[
+    {
+        nom: "Nombres",
+        image: "number.jpg"
+    },
+    {
+        nom: "Couleurs",
+        image: "color.jpg"
+    },
+    {
+        nom: "Alphabets",
+        image: "alphabet.jpg"
+    },
+    {
+        nom: "Formes",
+        image: "shape.jpg"
+    },
+    {
+        nom: "Solides",
+        image: "solid.jpg"
+    },
+                
+];
+// Cours
+[
+    {
+        titre: "Les nombres 0 à 10",
+        image: "number1.jpg",
+        video: "number1.mp4",
+        description: "On va apprendre à épeler et de voir comment les nombres 1 à 10 sont écrits: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 et 10"
+    },
+    
+    {
+        titre: "Les chiffres du Nouvel An ",
+        image: "number2.jpg",
+        video: "number2.mp4",
+        description: "On va apprendre à épeler et de voir comment les chiffres pour du Nouvel An sont écrits: les nombres entre 1 et 20"
+    },
+    {
+        titre: "Les nombres 0 à 10",
+        image: "number3.jpg",
+        video: "number3.mp4",
+        description: "On va apprendre à épeler et de voir comment les nombres 1 à 10 sont écrits: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 et 10"
+    },                            
+];
