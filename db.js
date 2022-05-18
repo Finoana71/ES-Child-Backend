5b694a17ef6d2d035469bca46c8cd51f159b9fdc
Finoana71 <finoanamandresy71@gmail.com>
2022-05-18 15:23:44 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,24 +1,24 @@
 // categorie 
 [
     {
-        nom: "Nombres",
-        image: "number.jpg"
+        "nom": "Nombres",
+        "image": "number.jpg"
     },
     {
-        nom: "Couleurs",
-        image: "color.jpg"
+        "nom": "Couleurs",
+        "image": "color.jpg"
     },
     {
-        nom: "Alphabets",
-        image: "alphabet.jpg"
+        "nom": "Alphabets",
+        "image": "alphabet.jpg"
     },
     {
-        nom: "Formes",
-        image: "shape.jpg"
+        "nom": "Formes",
+        "image": "shape.jpg"
     },
     {
-        nom: "Solides",
-        image: "solid.jpg"
+        "nom": "Solides",
+        "image": "solid.jpg"
     },
                 
 ];
@@ -26,21 +26,65 @@
 [
     {
         titre: "Les nombres 0 à 10",
-        image: "number1.jpg",
+        "image": "number1.jpg",
         video: "number1.mp4",
-        description: "On va apprendre à épeler et de voir comment les nombres 1 à 10 sont écrits: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 et 10"
-    },
-    
+        description: "<p>On va apprendre à épeler et de voir comment les nombres 1 à 10 sont écrits:</p>
+         <ul>
+             <li>0</li>
+             <li>1</li>
+             <li>2</li>
+             <li>3</li>
+             <li>4</li>
+             <li>5</li>
+             <li>6</li>
+             <li>7</li>
+             <li>8</li>
+             <li>9</li>
+             <li>10</li>
+         </ul>"
+    }, 
     {
         titre: "Les chiffres du Nouvel An ",
-        image: "number2.jpg",
+        "image": "number2.jpg",
         video: "number2.mp4",
         description: "On va apprendre à épeler et de voir comment les chiffres pour du Nouvel An sont écrits: les nombres entre 1 et 20"
     },
     {
         titre: "Les nombres 0 à 10",
-        image: "number3.jpg",
+        "image": "number3.jpg",
         video: "number3.mp4",
-        description: "On va apprendre à épeler et de voir comment les nombres 1 à 10 sont écrits: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 et 10"
-    },                            
+        description: "On va apprendre à épeler et de voir comment les nombres 1 à 10 sont écrits:   <ul>
+            <li>0</li>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li>7</li>
+            <li>8</li>
+            <li>9</li>
+            <li>10</li>
+        </ul>"
+    },
+    {
+        titre: "Les couleurs et les formes 1",
+        "image": "color1.jpg",
+        video: "color1.mp4",
+        description: "<p>On va apprendre des couleurs et des formes </p>
+        <p>Coleurs</p>
+        <ul>
+            <li>Rouge</li>
+            <li>Bleu</li>
+            <li>Vert</li>
+            <li>Jaune</li>
+        </ul>
+        <p>Formes</p>
+        <ul>
+            <li>Sphère</li>
+            <li>Carré</li>
+            <li>Rectangle</li>
+        </ul>
+        "
+    }                           
 ];
