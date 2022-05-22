9b7d36ed828a3e5919f364bbd04decc494713410
Finoana71 <77326289+Finoana71@users.noreply.github.com>
2022-05-22 17:48:45 +0300

    Cours vue liste, details

--- app/services/cours.service.js
+++ app/services/cours.service.js
@@ -17,9 +17,11 @@ async function findOne(req){
     let idCours = req.params.id;
     let cours = await Cours.findOne({_id: ObjectID(idCours)});
     let coursVue = await getCoursVue(req.id, idCours)
+    cours.vue = true;
     if(!coursVue){
         coursVue = { idCours: idCours, idUser: req.id, idCategorie: cours.idCategorie}    
         await CoursVue.insert(coursVue);
+        cours.vue = false;
     }
     return cours;
 }
@@ -73,7 +75,28 @@ async function getAllCours(req){
         cond.idCategorie = idCategorie;
     if(req.query.search)
         cond.titre = new RegExp(req.query.search, 'i')
-    return await Cours.find(cond).toArray();
+    let cours = await Cours.find(cond).toArray();
+    await setCoursVue(req.userId, idCategorie, cours);
+    return cours;
+}
+
+async function getCoursVue(idUser, idCategorie){
+    let cond = {idUser: idUser};
+    if(idCategorie)
+        cond.idCategorie = idCategorie;
+    return await CoursVue.find(cond).toArray()
+}
+
+async function setCoursVue(idUser, idCategorie, cours){
+    let coursVue = await getCoursVue(idUser, idCategorie);
+    // console.log(coursVue);
+    for(let i = 0; i < cours.length; i++){
+        cours[i].vue = false;
+        for(let j = 0; j < coursVue.length; j++){
+            if(coursVue[j].idCours == cours[i]._id)
+                cours[i].vue = true;
+        }
+    }
 }
 
 module.exports = {
